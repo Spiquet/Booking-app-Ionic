ce991ad9627573a7a9dbff3851c4750e2956aa3b
PIQUET SIMON <piquetsimon@Air-de-PIQUET.fritz.box>
2020-09-19 14:57:40 +0200

    feature : OutPutting places

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Place } from './place.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PlacesService {
+export class  PlacesService {
 
-  private Places = [];
+  public Places: Place[] = [
+    new Place ('p1', 'Manhattan Mansion', 'In the hearth of New York', 'https://images.unsplash.com/photo-1568515387631-8b650bbcdb90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', 100 ),
+    new Place ('p2', 'Lorient', 'In the hearth of Bretagne', 'https://images.unsplash.com/photo-1598109399096-408676f5d2fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', 100 ),
+    new Place ('p3', 'Lorient', 'In the hearth of Bretagne', 'https://images.unsplash.com/photo-1598109399096-408676f5d2fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60', 100 )
+
+
+  ];
 
   constructor() { }
 
