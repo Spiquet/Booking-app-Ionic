54f66a8d70c24ecc9dd4b9933d46397c47d934b5
PIQUET SIMON <piquetsimon@Air-de-PIQUET.fritz.box>
2020-09-20 10:47:16 +0200

    add-modal

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
@@ -8,7 +9,7 @@ import { AuthService } from './auth.service';
 })
 export class AuthPage implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -16,7 +17,8 @@ export class AuthPage implements OnInit {
 
   onLogin() {
 
-    this.authService.login(); 
+    this.authService.login();
+    this.router.navigateByUrl('places/tabs/discover');
 
 
   }
--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ModalController, NavController } from '@ionic/angular';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -8,12 +11,24 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  place: Place;
 
-  constructor(private router: Router, private navController: NavController) { }
+
+  constructor(private navController: NavController,
+              private modalCtrl: ModalController, private placesService: PlacesService,
+              private route: ActivatedRoute,
+              ) { }
 
 
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navController.navigateBack('/places/tabs/discover');
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
 
   onBookPlace() {
@@ -21,7 +36,24 @@ export class PlaceDetailPage implements OnInit {
     // this.router.navigateByUrl('/places/tabs/discover');
 
     // IonicStyle
-    this.navController.navigateBack('/places/tabs/discover');
+    // this.navController.navigateBack('/places/tabs/discover');
+
+    // ModalController allow to open modal by pointing to the desired compoment: here createBooking
+    this.modalCtrl.create({component :  CreateBookingComponent, componentProps: {selectedPlace : this.place}}).then(modalEl =>  {
+modalEl.present();
+return modalEl.onDidDismiss();
+
+    })
+    .then(resultData => {
+      console.log(resultData.data, resultData.role);
+      if (resultData.role === 'confirm') {
+        console.log('BOOKED!');
+      }
+    });
+
+
+
+
 
   }
 }
